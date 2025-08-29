d9a748d221113fee949d6ecaacf12e9fadf67830
TheBlackParrot <theblackparrot0@gmail.com>
2025-08-28 19:56:55 -0500

    add update notifications on game startup

---
+++ SpinRequests/Classes/ReleaseVersion.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SpinRequests.Classes;
+
+internal class ReleaseVersion
+{
+    [JsonProperty("tag_name")] public string? Version { get; set; }
+    [JsonProperty("prerelease")] public bool IsPreRelease { get; set; }
+    
+    [JsonConstructor]
+    public ReleaseVersion() {}
+}
--- SpinRequests/Plugin.cs
+++ SpinRequests/Plugin.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Logging;
+using Newtonsoft.Json;
 using SpinCore.Translation;
 using SpinRequests.Classes;
 using SpinRequests.Services;
 using SpinRequests.UI;
 using SpinShareLib;
+using UnityEngine;
 
 namespace SpinRequests;
 
@@ -56,6 +61,53 @@ public partial class Plugin : BaseUnityPlugin
     {
         QueueList.CreateQueueListPanel();
         Track.OnStartedPlayingTrack += TrackOnStartedPlayingTrack;
+        MainCamera.OnCurrentCameraChanged += MainCameraOnCurrentCameraChanged;
+    }
+
+    private static void MainCameraOnCurrentCameraChanged(Camera _)
+    {
+        MainCamera.OnCurrentCameraChanged -= MainCameraOnCurrentCameraChanged;
+        
+        Task.Run(async () =>
+        {
+            HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", $"{nameof(SpinRequests)}/{MyPluginInfo.PLUGIN_VERSION} (https://github.com/TheBlackParrot/SpinRequests)");
+            HttpResponseMessage responseMessage = await httpClient.GetAsync("https://api.github.com/repos/TheBlackParrot/SpinRequests/releases/latest");
+            responseMessage.EnsureSuccessStatusCode();
+            string json = await responseMessage.Content.ReadAsStringAsync();
+            
+            ReleaseVersion? releaseVersion = JsonConvert.DeserializeObject<ReleaseVersion>(json);
+            if (releaseVersion == null)
+            {
+                Log.LogInfo("Could not get newest release version information");
+                return;
+            }
+            if (releaseVersion.Version == null)
+            {
+                Log.LogInfo("Could not get newest release version information");
+                return;
+            }
+            if (releaseVersion.IsPreRelease)
+            {
+                Log.LogInfo("Newest release version is a pre-release");
+                return;
+            }
+            
+            Version currentVersion = new(MyPluginInfo.PLUGIN_VERSION);
+            Version latestVersion = new(releaseVersion.Version);
+            if (currentVersion < latestVersion)
+            {
+                Log.LogMessage($"{nameof(SpinRequests)} is out of date! (using v{currentVersion}, latest is v{latestVersion})");
+                
+                await Awaitable.MainThreadAsync();
+                NotificationSystemGUI.AddMessage(
+                    $"<b>{nameof(SpinRequests)}</b> has an update available! <alpha=#AA>(v{currentVersion} -> v{latestVersion})", 10f);
+            }
+            else
+            {
+                Log.LogMessage($"{nameof(SpinRequests)} is up to date!");
+            }
+        });
     }
 
     private void OnDisable()
