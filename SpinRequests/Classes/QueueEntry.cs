f1b5c432ef1aea7c6f9e9ca065edce4dcd67515c
TheBlackParrot <theblackparrot0@gmail.com>
2025-08-28 20:11:11 -0500

    only call `ScrollToTrack` if the menu is not open

--- SpinRequests/Classes/QueueEntry.cs
+++ SpinRequests/Classes/QueueEntry.cs
@@ -268,8 +268,11 @@ public class QueueEntry
 
                 if (!AlreadyDownloaded)
                 {
-                    XDSelectionListMenu.Instance.ScrollToTrack(PlayerSettingsData.Instance.LastPlayedTrackHandle);
-                    
+                    if (!XDSelectionListMenu.Instance.ShouldBeOpen)
+                    {
+                        XDSelectionListMenu.Instance.ScrollToTrack(PlayerSettingsData.Instance.LastPlayedTrackHandle);
+                    }
+
                     NotificationSystemGUI.AddMessage($"Downloading map {SpinShareKey}...", 5f);
                     
                     await Plugin.SpinShare.downloadSongAndUnzip(SpinShareKey.ToString(), Plugin.CustomsPath);
